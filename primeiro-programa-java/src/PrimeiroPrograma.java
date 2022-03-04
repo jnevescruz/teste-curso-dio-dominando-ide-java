d486f18edf2aeb5e237fd059f2ac33eafcc5ae5e
jnevescruz <jnevescruz1@gmail.com>
2022-03-04 15:44:04 -0300

    teste-curso-dio-dominando-ide-java

---
+++ primeiro-programa-java/src/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		
+		
+		
+		
+		
+		
+		
+		/*int a = 2;
+		int b = 3;
+		
+		System.out.println("Hello World " + (a + b));*/
+	}
+
+}
